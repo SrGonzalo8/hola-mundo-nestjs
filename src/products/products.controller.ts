7f03d23ebaff51de5a3a470a484eed4ff62caf58
SrGonzalo8 <gonzaloinformatica8@gmail.com>
2023-03-24 14:33:30 +0100

    controladores metodos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, HttpCode, HttpStatus, Res, Put } from '@nestjs/common';
 @Controller('products')
 export class ProductsController {
 @Get()
@@ -9,17 +9,41 @@ return "Estamos en productos!!!";
 getSpecialProducts(): string {
 return "Te vamos a mostrar los productos más calientes!!";
 }
+
 @Get(':id')
-find( @Param() params) {
-return `Estás consultando el producto ${params.id}`;
+find(@Res() response, @Param('id') id: number) {
+if(id < 100) {
+return response.status(HttpStatus.OK).send(`Página del producto ${id}`);
+} else {
+return response.status(HttpStatus.NOT_FOUND).send(`Producto inexistente`);
 }
+}
+
 @Get(':id/:size')
 findWithSize(@Param('id') id: number, @Param('size') size: string ) {
 return `Página de detalle de producto ${id}, en tamaño ${size}`;
 }
+@Post()
+createProduct(
+@Body('name') name: string,
+@Body('description') description: string
+) {
+return `Creo el producto ${name} con descripción ${description}.`;
+}
 
 @Post()
-createProduct(@Body() body) {
-return `Creo un producto ${body.name} con descripción ${body.description}`;
+@HttpCode(HttpStatus.NO_CONTENT)
+NoContentProduct(@Body() body) {
+return body;
+}
+@Get('ruta-error-404')
+@HttpCode(HttpStatus.NOT_FOUND)
+rutaConError404() {
+return 'Esto es un error 404!!';
+}
+@Put(':id')
+update(@Param('id') id: number, @Body() body) {
+return `Estás haciendo una operación de actualización del recurso ${id}
+con ${body.name} y ${body.description}`;
 }
 }
