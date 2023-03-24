3414ef29ac53e26c30642bfffd463f7069fa883a
luis <langel7630@gmail.com>
2023-03-24 11:54:04 +0100

    controladores

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('API Afilia3 by A3Satel')
+    .setDescription('Api prueba')
+    .setVersion('1.0')
+    .build();
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/products/products.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('products')
+export class ProductsController {
+  @Get()
+  getHelloInProducts(): string {
+    return 'Estamos en productos!!!';
+  }
+
+  @Get('hot')
+  getSpecialProducts(): string {
+    return 'Te vamos a mostrar los productos más calientes!!';
+  }
+
+  @Get(':id')
+  find(@Param() params) {
+    return `Estás consultando el producto ${params.id}`;
+  }
+
+  @Get(':id/:size')
+  findWithSize(@Param('id') id: number, @Param('size') size: string) {
+    return `Página de detalle de producto ${id}, en tamaño ${size}`;
+  }
+
+  @Post()
+  createProduct() {
+    return 'Estamos atendiendo una solicitud de tipo Post';
+  }
+}
